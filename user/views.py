392ff4e9f2d92f35415b5cfc0efc7bdbcd4bfa57
utsabmaharjan <maharjanutsab12@gmail.com>
2024-07-10 18:51:02 +0545

    trans/updatebalance

--- user/views.py
+++ user/views.py
@@ -6,8 +6,8 @@ from user.forms import UserBalanceForm
 from transaction.service import TransactionService
 from transaction.models import Transaction
 from django.db import transaction
+from user.services import UserService
 # Create your views here.
-
 @login_required
 @transaction.atomic
 def admin_add_balance(request):
@@ -29,14 +29,37 @@ def admin_add_balance(request):
     }
     return render(request, 'user/balance-create.html',context)
 
-@login_required
+
 def list_user_balance(request):
     user_balance = UserBalance.objects.all()
     context = {
         'user_balance':user_balance
     }
-    return render(request, 'user/balance-list.html',context)
+    return render(request, 'users/balance-list.html',context)
 
 @login_required
-def update_balance(request, id):
+@transaction.atomic
+def update_balance(request,id):
     user_balance = UserBalance.objects.get(id=id)
+    error = {}
+    if request.method == "POST":
+        if int(request.POST['balance'])<0:
+            error['form']="Value must be positive" 
+        UserService.add_balance(user_balance, int(request.POST['balance']))
+        TransactionService.create_transaction(
+                "Update balance",
+                user_balance.user.id,
+                request.POST['balance'],
+                Transaction.Transactionstatus.COMPLETED,
+                Transaction.PaymentMethod.ADMIN,
+                Transaction.TransactionType.INCOME
+
+            )
+        
+        return redirect('/user/userbalance')
+    
+    context = {
+        "error":error
+    }
+    return render(request, 'users/update_balance.html',context)
+
